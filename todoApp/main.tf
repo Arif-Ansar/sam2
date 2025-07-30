94575a2f4ac6b42fd1519f3cb5ff2d3758187380
Arif-Ansar <mojaddiyun25693@gmail.com>
2025-07-30 14:55:31 +0530

    Update main.tf

--- todoApp/main.tf
+++ todoApp/main.tf
@@ -7,6 +7,7 @@ module "resource_group2"{
     source = "../modules/azurerm_resource_group"
     resource_group_location = "centralindia"
     resource_group_name = "tikks3"
+}
 module "resource_group1"{
     source = "../modules/azurerm_resource_group"
     resource_group_location = "centralindia"
