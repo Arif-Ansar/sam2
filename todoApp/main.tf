e82fdc6bbc92c1c215d0d5d7c5ac09386a8bc180
Arif Ansari <ansari786mj@gmail.com>
2025-07-30 12:01:33 +0530

    rg tikks3 added

--- todoApp/main.tf
+++ todoApp/main.tf
@@ -3,7 +3,11 @@ module "resource_group"{
     resource_group_location = "centralindia"
     resource_group_name = "tikks1"
 }
-
+module "resource_group2"{
+    source = "../modules/azurerm_resource_group"
+    resource_group_location = "centralindia"
+    resource_group_name = "tikks3"
+}
 module "virtual_network"{
     depends_on = [module.resource_group]
     source = "../modules/azurerm_virtual_network"
